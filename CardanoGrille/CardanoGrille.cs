623fde10e18708465cb0591297e482b0cbca5c28
Bogdan Peleh <peleh.b97@gmail.com>
2018-04-03 14:16:29 +0300

    add decode text

--- CardanoGrille/CardanoGrille.cs
+++ CardanoGrille/CardanoGrille.cs
@@ -14,6 +14,15 @@ namespace CardanoGrille
 
     class CardanoGrille
     {
+        private const string _messageError = "Некоректне введення!!!";
+        private const string _messageIsCorrect = "Цей текст можна вважати коректним?";
+        private const string _messageYes = "1:Так";
+        private const string _messageNo = "2:Ні";
+        private const int _repeatAfterNoCorrectAns = 5;
+        private const int _userSayYes = 1;
+        private const int _userSayNo = 2;
+        private const int _sizePartText = 10;
+
         public StringBuilder EncodingUA(StringBuilder inputText, GrilleKey key){
             StringBuilder res = new StringBuilder();
 
@@ -40,6 +49,16 @@ namespace CardanoGrille
 
         public StringBuilder DecodingWithoutKey(StringBuilder inputText){
             StringBuilder res = new StringBuilder();
+            foreach (GrilleKey grilleKey in GrilleKeys.Keys)
+            {
+                StringBuilder resultText = DecodingUA(inputText, grilleKey);
+                StringBuilder partText = GetFirstPart(resultText);
+                if (CorrectText(partText))
+                {
+                    res = resultText;
+                    break;
+                }
+            }
 
             return res;
         }
@@ -50,14 +69,14 @@ namespace CardanoGrille
             inputText = TextAnalyser.RemoveSpace(inputText);
             inputText = TextAnalyser.RemovePunctuation(inputText);
 
-            int size = GrilleKeys.Keys[0].Key.Count* GrilleKeys.Keys[0].Key.Count;
+            int size = GrilleKeys.Keys[0].Key.Count * GrilleKeys.Keys[0].Key.Count;
 
             res = TextAnalyser.SplitToSize(inputText, size);
 
             return res;
         }
-        private List<StringBuilder> TextPreparationDecoding(StringBuilder inputText)
-        {
+
+        private List<StringBuilder> TextPreparationDecoding(StringBuilder inputText){
             List<StringBuilder> res = new List<StringBuilder>();
 
             int size = GrilleKeys.Keys[0].Key.Count * GrilleKeys.Keys[0].Key.Count;
@@ -83,16 +102,16 @@ namespace CardanoGrille
                 }
                 else
                 {
-                    rotateMatrix = MatrixService.Multiply(MatrixHelpCardoneAlgoritm.InvertingMatrix,rotateMatrix);
+                    rotateMatrix = MatrixService.Multiply(MatrixHelpCardoneAlgoritm.InvertingMatrix, rotateMatrix);
                 }
             }
 
             return MatrixService.ToVector(res);
         }
-        private StringBuilder FullString(StringBuilder inputText, List<int> vectorNumber)
-        {
+
+        private StringBuilder FullString(StringBuilder inputText, List<int> vectorNumber){
             StringBuilder res = new StringBuilder("                ");
-           
+
             for (int i = 0; i < inputText.Length; i++)
             {
                 int index = vectorNumber.IndexOf(i + 1);
@@ -102,8 +121,7 @@ namespace CardanoGrille
             return res;
         }
 
-        private StringBuilder FullStringReverse(StringBuilder inputText, List<int> vectorNumber)
-        {
+        private StringBuilder FullStringReverse(StringBuilder inputText, List<int> vectorNumber){
             StringBuilder res = new StringBuilder("                ");
 
             for (int i = 0; i < inputText.Length; i++)
@@ -113,6 +131,47 @@ namespace CardanoGrille
             }
 
             return res;
+        }
+
+        private bool CorrectText(StringBuilder inputString){
+            int i = 0;
+            Console.WriteLine(inputString);
+            Console.WriteLine(_messageIsCorrect);
+            Console.WriteLine(_messageYes);
+            Console.WriteLine(_messageNo);
+            while (true)
+            {
+                try
+                {
+                    int number = Int32.Parse(Console.ReadLine());
+                    if (number == _userSayYes)
+                    {
+                        return true;
+                    }
+                    else if (number == _userSayNo)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        throw new Exception();
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine(_messageError);
+                    i++;
+                    if (i == _repeatAfterNoCorrectAns)
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        private static StringBuilder GetFirstPart(StringBuilder inputLetter){
+            StringBuilder tempInputString = new StringBuilder(inputLetter.ToString());
+            return tempInputString.Remove(_sizePartText, tempInputString.Length - _sizePartText);
         }
     }
 }
